8fea4e0c3abcb2fae6ab31eb2f234271071fb83b
TejaswiniDonakonda <dtejaswini9977@gmail.com>
2024-08-27 08:17:35 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score=33
+
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber));
+
+//"33"=>number
+
+let isLoggedIn="abc"
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+//1=>true
+//""=>false
+//"teju"=>true
+
+let someNumber=22
+//console.log(typeof(someNumber));
+let stringNumber=String(someNumber);
+//console.log(typeof(stringNumber));
+//===============================operations=========================
+let str1="SUM";
+let str2=20;
+let str3=(5+5+"2"+3+2);
+console.log(str3)
