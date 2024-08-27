36d1d78391d5b4b80895efab4e769c94f3ea1e1d
TejaswiniDonakonda <dtejaswini9977@gmail.com>
2024-08-27 10:41:34 +0000

    dateandtime

---
+++ 01_basics/07_Date_Time.js
@@ -0,0 +1,11 @@
+//Dates
+
+let myDate=new Date();
+
+console.log(myDate);//2024-08-27T10:37:41.886Z
+
+console.log(myDate.toISOString());//2024-08-27T10:37:41.886Z
+console.log(myDate.toJSON()); //2024-08-27T10:37:41.886Z
+console.log(myDate.toLocaleString());//8/27/2024, 10:37:41 AM
+console.log(myDate.toString());//Tue Aug 27 2024 10:37:41 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString());//Tue Aug 27 2024
