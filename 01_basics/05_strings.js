54fea16c6730ec5e2421710c7c1d015b06e498de
TejaswiniDonakonda <dtejaswini9977@gmail.com>
2024-08-27 09:44:39 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+let name="Kridha";
+let age=2;
+
+console.log(`Hey iam ${name} and Iam ${age} years old.`);
+
+const newName=new String('Vaasudeva');
+console.log(newName.length);
+console.log(newName.toUpperCase())
+console.log(newName.substring(0,5).toLowerCase())
+
+
