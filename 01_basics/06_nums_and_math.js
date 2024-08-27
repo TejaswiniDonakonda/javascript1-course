9202a6c34d84aba61ea162f34935ac5f102c80fc
TejaswiniDonakonda <dtejaswini9977@gmail.com>
2024-08-27 10:31:48 +0000

    numsAndMath

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,18 @@
+let score=400;
+//console.log(score);
+
+let newScore= new Number(100);
+//console.log(newScore)
+//console.log(newScore.toFixed(2))//100.00
+
+let num=123.7878;
+//console.log(num.toPrecision(3));
+
+console.log(Math.abs(-4))
+console.log(Math.round(4.6));//5 if it is 4.3=> 4
+console.log(Math.ceil(4.2));//5
+console.log(Math.floor(4.9))//4
+
+let min=10;
+let max=20;
+ console.log(Math.floor(Math.random()*(max-min+1))+min);
