c38c6adbbf5a6990968919566920d2f15db2210c
TejaswiniDonakonda <dtejaswini9977@gmail.com>
2024-08-27 07:28:26 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=1234;
+let accountEmail="abc@gmail.com";
+var accountPassword='abcd';
+accountCity="ATP";
+
+accountEmail="1235@gmail.com";
+accountPassword="123456"
+accountCity="MDK";
+
+console.log(accountPassword);
+console.table([accountEmail,accountCity,accountId])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; 
+
+let name="Krishna";
+let age=3;
+let state=null;
+let password;
+
+//types
+//number=>2 to power 53
+//string
+//bigInt
+//boolean
+//symbol
+//null 
+//undefined
+//object
+console.log(typeof(name))
+console.log(typeof(age))
+//console.log(typeof(state))
+console.log(typeof(null))
+console.log(typeof(undefined))
+//console.log(typeof(password))
+
